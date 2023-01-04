a288d2792d48ec705496c48ebb096e007e937641
lisknonanika <lisknonanika@gmail.com>
2023-01-04 15:51:28 +0900

    upd

--- src/api.ts
+++ src/api.ts
@@ -8,29 +8,29 @@ const port = EXP_CONFIG.port;
 const exp = express();
 exp.use(cors({ origin: true, credentials: true }));
 
-exp.get('/', async(_req, _res) => {
+exp.get('/', async (_req, _res) => {
     const resultPing = await RunPing(false);
     const resultFetch = await RunFetch(false);
-    _res.json({ ping: resultPing, fetch: resultFetch});
+    _res.json({ ping: resultPing, fetch: resultFetch });
 });
 
-exp.get('/history', async(_req, _res) => {
-    const resultPing:any[] = [];
+exp.get('/history', async (_req, _res) => {
+    const resultPing: any[] = [];
     for (let host of PING_CONFIG.hosts) {
-        const historyData:{host:string, data:PING_DATA[]} = {host:host, data:[]};
+        const historyData: { host: string, data: PING_DATA[] } = { host: host, data: [] };
         const histories = PingHistory.reverse().filter(data => data.host === host);
         for (let history of histories) historyData.data.push(history.data);
         resultPing.push(historyData);
     }
 
-    const resultFetch:any[] = [];
+    const resultFetch: any[] = [];
     for (let url of FETCH_CONFIG.url) {
-        const historyData:{url:string, data:FETCH_DATA[]} = {url:url, data:[]};
+        const historyData: { url: string, data: FETCH_DATA[] } = { url: url, data: [] };
         const histories = FetchHistory.reverse().filter(data => data.url === url);
         for (let history of histories) historyData.data.push(history.data);
         resultFetch.push(historyData);
     }
-    _res.json({ ping: resultPing, fetch: resultFetch});
+    _res.json({ ping: resultPing, fetch: resultFetch });
 });
 
 exp.listen(port, () => {
--- src/app.ts
+++ src/app.ts
@@ -4,46 +4,46 @@ import AbortController from 'abort-controller';
 
 import { PING as PING_CONFIG, FETCH as FETCH_CONFIG } from './config.json';
 
-export type PING_DATA = {alive:boolean, timestamp:Date};
-export type FETCH_DATA = {alive:boolean, timestamp:Date};
-export type PING_HISTORY = {host:string, data:PING_DATA};
-export type FETCH_HISTORY = {url:string, data:FETCH_DATA};
-export type PING_RESULT = {host:string, ip:string, alive:boolean, downTime:number};
-export type FETCH_RESULT = {url:string, status:string, alive:boolean, downTime:number};
+export type PING_DATA = { alive: boolean, timestamp: Date };
+export type FETCH_DATA = { alive: boolean, timestamp: Date };
+export type PING_HISTORY = { host: string, data: PING_DATA };
+export type FETCH_HISTORY = { url: string, data: FETCH_DATA };
+export type PING_RESULT = { host: string, ip: string, alive: boolean, downTime: number };
+export type FETCH_RESULT = { url: string, status: string, alive: boolean, downTime: number };
 
-export const PingHistory:PING_HISTORY[] = [];
-export const FetchHistory:FETCH_HISTORY[] = [];
+export const PingHistory: PING_HISTORY[] = [];
+export const FetchHistory: FETCH_HISTORY[] = [];
 
-export const RunPing = async(isCron:boolean) => {
-    const result:PING_RESULT[] = [];
+export const RunPing = async (isCron: boolean) => {
+    const result: PING_RESULT[] = [];
     for (let host of PING_CONFIG.hosts) {
-        let ip:string = "***.***.***.***";
-        let alive:boolean = false;
+        let ip: string = "***.***.***.***";
+        let alive: boolean = false;
 
         try {
             const ret = await ping.promise.probe(host, PING_CONFIG.option);
             if (ret.numeric_host !== undefined && ret.numeric_host !== "NA") ip = ret.numeric_host;
             alive = ret.alive;
-        } catch(_err) {
+        } catch (_err) {
             // none
         }
-        result.push({host: host, ip: ip, alive: alive, downTime: getPingDownTime(host, alive)});
-        if (isCron) setPingHistory(host, ip, alive);
+        result.push({ host: host, ip: ip, alive: alive, downTime: getPingDownTime(host, alive) });
+        if (isCron) setPingHistory(host, alive);
     }
     return result;
 }
 
-export const RunFetch = async(isCron:boolean) => {
-    const result:FETCH_RESULT[] = [];
+export const RunFetch = async (isCron: boolean) => {
+    const result: FETCH_RESULT[] = [];
     for (let url of FETCH_CONFIG.url) {
         const controller = new AbortController();
-        const fetchTimeout = setTimeout(() => { controller.abort();}, FETCH_CONFIG.timeout);
-        
-        let status:string = "***";
-        let alive:boolean = false;
+        const fetchTimeout = setTimeout(() => { controller.abort(); }, FETCH_CONFIG.timeout);
+
+        let status: string = "***";
+        let alive: boolean = false;
 
         try {
-            const response = await fetch(url, {signal: controller.signal});
+            const response = await fetch(url, { signal: controller.signal });
             status = response.status.toString();
             alive = response.ok;
         } catch (_err) {
@@ -51,32 +51,32 @@ export const RunFetch = async(isCron:boolean) => {
         } finally {
             clearTimeout(fetchTimeout);
         }
-        result.push({url: url, status: status, alive: alive, downTime: getFetchDownTime(url, alive)});
-        if (isCron)  setFetchHistory(url, status, alive);
+        result.push({ url: url, status: status, alive: alive, downTime: getFetchDownTime(url, alive) });
+        if (isCron) setFetchHistory(url, alive);
     }
     return result;
 }
 
-const setPingHistory = (host:string, ip:string, alive:boolean) => {
-    const findData:PING_HISTORY|undefined = PingHistory.reverse().find(data => data.host === host);
+const setPingHistory = (host: string, alive: boolean) => {
+    const findData: PING_HISTORY | undefined = PingHistory.reverse().find(data => data.host === host);
     if (findData !== undefined && findData.data.alive === alive) return;
-    PingHistory.push({host: host, data: {alive: alive, timestamp: new Date()}});
+    PingHistory.push({ host: host, data: { alive: alive, timestamp: new Date() } });
 }
 
-const setFetchHistory = (url:string, status:string, alive:boolean) => {
-    const findData:FETCH_HISTORY|undefined = FetchHistory.reverse().find(data => data.url === url);
+const setFetchHistory = (url: string, alive: boolean) => {
+    const findData: FETCH_HISTORY | undefined = FetchHistory.reverse().find(data => data.url === url);
     if (findData !== undefined && findData.data.alive === alive) return;
-    FetchHistory.push({url: url, data: {alive: alive, timestamp: new Date()}});
+    FetchHistory.push({ url: url, data: { alive: alive, timestamp: new Date() } });
 }
 
-const getPingDownTime = (host:string, alive:boolean):number => {
-    const findData:PING_HISTORY|undefined = PingHistory.reverse().find(data => data.host === host);
+const getPingDownTime = (host: string, alive: boolean): number => {
+    const findData: PING_HISTORY | undefined = PingHistory.reverse().find(data => data.host === host);
     if (findData === undefined || findData.data.alive || alive) return 0;
-    return Math.floor((new Date().getTime() - findData.data.timestamp.getTime())/60000);
+    return Math.floor((new Date().getTime() - findData.data.timestamp.getTime()) / 60000);
 }
 
-const getFetchDownTime = (url:string, alive:boolean):number => {
-    const findData:FETCH_HISTORY|undefined = FetchHistory.reverse().find(data => data.url === url);
+const getFetchDownTime = (url: string, alive: boolean): number => {
+    const findData: FETCH_HISTORY | undefined = FetchHistory.reverse().find(data => data.url === url);
     if (findData === undefined || findData.data.alive || alive) return 0;
-    return Math.floor((new Date().getTime() - findData.data.timestamp.getTime())/60000);
+    return Math.floor((new Date().getTime() - findData.data.timestamp.getTime()) / 60000);
 }
